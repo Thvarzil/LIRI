00f11171b844c9e88139694c598d3f33e4e5b56f
Louis LeBohec <louisblebohec@gmail.com>
2017-08-23 11:00:24 -0600

    file includes successful functionality tests

--- liri.js
+++ liri.js
@@ -12,29 +12,43 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
     id: "746610783bb24eaebef0335184e3586a",
     secret: "21c1ad91a898400d87c76fec1a088a53"
-})
+});
+
+//require request package for OMDb scraping
+var request = require("request");
+
+//require file structure package
+var fs = require("fs");
 
 //Stores command line command in command var
 var command = process.argv[2];
 
-function userInput(){
-    if(command==="my-tweets"){
+function userInput() {
+    if (command === "my-tweets") {
+        console.log("Your 20 most recent tweets:");
 
     }
-    if(command==="spotify-this-song"){
+    else if (command === "spotify-this-song") {
+        console.log("Song Info:");
+    }
+    else if (command === "movie-this") {
+        console.log("Movie Info:");
 
     }
-    if(command==="movie-this"){
-
+    else if (command === "do-what-it-says") {
+        var randText;
+        fs.readFile('./random.txt', function (err, data) {
+            if (err) throw err;
+            console.log(data);
+            randText = JSON.stringify(data);
+            console.log("The command from random.txt was: " + randText);
+        });
     }
-    if(command==="do-what-it-says"){
-
-    }
-    else{
+    else {
         console.log("The command you entered was not understood. Please Enter one of the following commands:");
         console.log("--> my-tweets : Displays the last 20 tweets you have posted");
         console.log("--> spotify-this-song 'song title' : Displays Spotify info on the entered song");
-        console.log("--> movie-this 'movie title' : Displays IMDb info on selected movie. If no title is entered, defaults to Mr. Nobody")
+        console.log("--> movie-this 'movie title' : Displays IMDb info on selected movie. If no title is entered, defaults to Mr. Nobody");
         console.log("--> do-what-it-says : Uses the contents of random.txt as the inputted command");
     }
 }
