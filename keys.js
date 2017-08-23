497079953d5f18a63938de4631d7a6d6fcc55215
Louis LeBohec <louisblebohec@gmail.com>
2017-08-23 10:13:51 -0600

    first commit

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+//Check to make sure we are loaded
+console.log("keys are loaded my dude");
+
+//Twitter API keys
+exports.twitterKeys = {
+    consumer_key: '<input here>',
+    consumer_secret: '<input here>',
+    access_token_key: '<input here>',
+    access_token_secret: '<input here>',
+}
