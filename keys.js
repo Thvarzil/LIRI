250ec6a974112ab83a2a7ec2583e7a236117f628
Louis LeBohec <louisblebohec@gmail.com>
2017-08-23 18:49:14 -0600

    progress commit

--- keys.js
+++ keys.js
@@ -1,11 +1,14 @@
+var Twitter = require("twitter");
+
 
 //Check to make sure we are loaded
 console.log("keys are loaded my dude");
 
 //Twitter API keys
-exports.twitterKeys = {
+exports.twitterKeys = new Twitter({
     consumer_key: 'DZFXiwBsA2VXiEolWq8VvUNXi',
     consumer_secret: 'pprcR0e9A9uv5Zy0ptSqZxrkI6LZiClCQcYwmrJhmH6fZNRHni',
     access_token_key: '900387772367724544-3kpESm9qfPmQuHZMGCAXMh1OAc1RAuv',
-    access_token_secret: '\tgUWLS8laYPeHDwVLAIopN7tlM1LfAPixkHvvzkaSXj9pf\n'
-};
+    access_token_secret: 'gUWLS8laYPeHDwVLAIopN7tlM1LfAPixkHvvzkaSXj9pf'
+});
+
--- liri.js
+++ liri.js
@@ -1,10 +1,13 @@
+
+var jQuery = require("jquery");
+var ajax = require("ajax");
 //Stores twitter keys from keys.js into a variable
 
 //Require keys.js
 const keys = require('./keys.js');
 
 //stores keys in twitKeys
-var twitKeys = keys.twitterKeys;
+var twitter = keys.twitterKeys;
 
 //Require and set up spotify API
 var Spotify = require('node-spotify-api');
@@ -13,6 +16,8 @@ var spotify = new Spotify({
     id: "746610783bb24eaebef0335184e3586a",
     secret: "21c1ad91a898400d87c76fec1a088a53"
 });
+
+
 
 //require request package for OMDb scraping
 var request = require("request");
@@ -27,9 +32,46 @@ function userInput() {
     if (command === "my-tweets") {
         console.log("Your 20 most recent tweets:");
 
+        twitter.get('statuses/user_timeline', {screen_name: 'liri_alias', count: 20}, getData);
+        function getData(err, data, response) {
+            // if(err) throw err
+
+            for(var i = 0; i<data.length; i++){
+                console.log(data[i].text);
+                console.log(data[i].created_at);
+            }
+        }
+
     }
     else if (command === "spotify-this-song") {
-        console.log("Song Info:");
+
+        var songTitle = process.argv[3];
+
+        if(songTitle===""){
+            songTitle = "The Sign";
+        }
+
+
+        spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            var results = data.tracks.items.length;
+            console.log("Your search returned " + results + " results!");
+
+            for(var i = 0; i<data.tracks.items.length; i++){
+                console.log("\bSong Info  :\n");
+
+                console.log("Title      : " + data.tracks.items[i].name);
+                // for(var j = 0; j < data.tracks.items[i].artists[j].name){
+                console.log("Artist     : " + data.tracks.items[i].album.artists[0].name);
+                // }
+                console.log("Album      : " + data.tracks.items[i].album.name);
+                console.log("Preview URL: " + data.tracks.items[i].preview_url);
+                console.log("--------------------------------------------")
+            }
+
+        });
     }
     else if (command === "movie-this") {
         console.log("Movie Info:");
